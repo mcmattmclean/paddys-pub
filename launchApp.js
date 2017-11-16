54d2a4a197ca842ad25254f7ac6c498d24842608
Matthew McLean <matthewmclean21@gmail.com>
2017-11-16 11:38:38 -0800

    Update port to match OSU server

--- launchApp.js
+++ launchApp.js
@@ -5,7 +5,7 @@ var handlebars = require('express-handlebars').create({defaultLayout: 'main'});
 var bodyParser = require('body-parser');
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
-app.set('port', 3000);
+app.set('port', 7728);
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use('/static', express.static('public'));
