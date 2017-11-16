fceed920af5bbe061580e287853a272e80ba002d
Matthew McLean <matthewmclean21@gmail.com>
2017-11-16 11:28:53 -0800

    Move regenBeers to bottom of file

--- launchApp.js
+++ launchApp.js
@@ -1,35 +1,4 @@
-// Generate product list, to be replaced by MySQL at a later time
-function regenBeers(){
-    var adjectives = ["Spicy", "Rowdy", "Glorious", "Humble", "Saucy", "Unnecessary", "Cantankerous", "Bodacious", "Yellow-bellied", "Blue-footed", "Poorly-made", "Ill-conceived"];
-    var nouns = ["Moonshine", "Citrus", "Nasty", "Boffish", "Stank", "Mocha", "Celebration", "Watermelon", "Bourbon", "Classic", "California-Style"];
-    var types = ["IPA", "Imperial IPA", "Lager", "Stout", "Porter", "Pale Ale", "Sour"];
-    var availabilities = ["Year-round", "Seasonal", "Limited"];
-    var products = [];
-    var numProducts = types.length;
-    for(var i = 0; i < numProducts; i++){
-        var type = types[i];
-        // Get random word for noun and remove
-        var nounIndex = Math.floor(Math.random() * nouns.length);
-        var noun = nouns[nounIndex];
-        nouns.splice(nounIndex, 1);
-        // Get random word for adjective and remove
-        var adjIndex = Math.floor(Math.random() * adjectives.length);
-        var adjective = adjectives[adjIndex];
-        adjectives.splice(adjIndex, 1);
-        var newProduct = {}
-        newProduct.name = adjective + ' ' + noun + ' ' + type;
-        newProduct.type = type;
-        newProduct.description = "Everything you need to know about our amazing " + newProduct.name + ":\nLorem ipsum dolor sit amet consectetur adipisicing elit. Quidem minima tempora asperiores, ipsum quae ipsam blanditiis, at aliquam ex dolorum delectus dolore ut quo fugit explicabo ipsa tenetur, cumque ducimus?";
-        newProduct.id = i + 1;
-        newProduct.availability = availabilities[Math.floor(Math.random() * availabilities.length)];
-        newProduct.alc = (Math.random() * (12 - 4 + 1) + 4).toFixed(1);
-        newProduct.ibu = Math.floor(Math.random() * (100 - 45 + 1) + 45);
-        products.push(newProduct);
-    }
-    return products;    
-}
 var storeData = {};
-
 var express = require('express');
 var app = express();
 var handlebars = require('express-handlebars').create({defaultLayout: 'main'});
@@ -75,3 +44,34 @@ app.use(function(err, req, res, next){
 app.listen(app.get('port'), function(){
     console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
 });
+
+// Generate product list, to be replaced by MySQL at a later time
+function regenBeers(){
+    var adjectives = ["Spicy", "Rowdy", "Glorious", "Humble", "Saucy", "Unnecessary", "Cantankerous", "Bodacious", "Yellow-bellied", "Blue-footed", "Poorly-made", "Ill-conceived"];
+    var nouns = ["Moonshine", "Citrus", "Nasty", "Boffish", "Stank", "Mocha", "Celebration", "Watermelon", "Bourbon", "Classic", "California-Style"];
+    var types = ["IPA", "Imperial IPA", "Lager", "Stout", "Porter", "Pale Ale", "Sour"];
+    var availabilities = ["Year-round", "Seasonal", "Limited"];
+    var products = [];
+    var numProducts = types.length;
+    for(var i = 0; i < numProducts; i++){
+        var type = types[i];
+        // Get random word for noun and remove
+        var nounIndex = Math.floor(Math.random() * nouns.length);
+        var noun = nouns[nounIndex];
+        nouns.splice(nounIndex, 1);
+        // Get random word for adjective and remove
+        var adjIndex = Math.floor(Math.random() * adjectives.length);
+        var adjective = adjectives[adjIndex];
+        adjectives.splice(adjIndex, 1);
+        var newProduct = {}
+        newProduct.name = adjective + ' ' + noun + ' ' + type;
+        newProduct.type = type;
+        newProduct.description = "Everything you need to know about our amazing " + newProduct.name + ":\nLorem ipsum dolor sit amet consectetur adipisicing elit. Quidem minima tempora asperiores, ipsum quae ipsam blanditiis, at aliquam ex dolorum delectus dolore ut quo fugit explicabo ipsa tenetur, cumque ducimus?";
+        newProduct.id = i + 1;
+        newProduct.availability = availabilities[Math.floor(Math.random() * availabilities.length)];
+        newProduct.alc = (Math.random() * (12 - 4 + 1) + 4).toFixed(1);
+        newProduct.ibu = Math.floor(Math.random() * (100 - 45 + 1) + 45);
+        products.push(newProduct);
+    }
+    return products;    
+}
